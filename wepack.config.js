1641e7b53650aa94cb08fa3802b325975dbc09f4
Gabriela Muñoz <mikaela98501@gmail.com>
2024-07-26 12:55:49 -0600

    Estructura y config del proyecto

---
+++ wepack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', //Punto de entrada de la aplicacion 
+  output: {
+     filename: 'bundle.js', // Nombre del archivo de salida
+     path: path.resolve(__dirname, 'dist'), //Carpeta de salida
+    },
+  module: {
+       rules:[
+        {
+            test: /\.css$/, //Regex, para identificar archivos CSS
+            use: [ 'style-loader'], //Loaders para procesar archivos CSS
+        },
+          {
+            test: /\.js$/, //Regex, para identificar archivos JS
+            exclude: /node_modules/, //Excluir la carpeta node_modules
+            use: {
+                loader: 'babel-loader', //Loader para pasar JS moderno a JS mas antiguo para todos los navegadores
+                options: {
+                    presets: ['@babel/preset-env'], 
+                },
+            },
+          },
+       ],
+    },
+    devtool: 'source-map', //Generar source maps para facilitar la depuracion
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'), //Carpeta principal del servidor
+        compress: true, //Habilitar la compresión gzip
+        port: 9000, //Puerto del servidor de desarrollo
+    },
+};
